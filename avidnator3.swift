a6ffd13d38283490d8bf78921a5a5a2b44e98fc4
Don Schuette <dvsmusic@gmail.com>
2024-08-26 16:22:10 -0700

    Create avidnator3.swift

---
+++ avidnator3.swift
@@ -0,0 +1,28 @@
+// Avidnator 3 code
+
+// note: specify folder options?
+
+// possibly have user input Show name, date for folder creation
+
+
+// get source directory path from user input
+
+
+// get destination directory path from user input
+
+
+// read metadata from each file in source directory, spcifically timecode (BWF)
+
+// create folders in destination directory based on timecode (BWF) under the Show name and date folders
+
+// copy files from source directory to destination directory based on timecode (BWF)
+
+// iterate through each file in destination directory and append track number to end of file name
+
+// note: can we use drag and drop from desktop for source and destination directories?
+
+// nate: can this be a web app? Is javascript the best language for this?
+
+// note: can we use a GUI for user input?
+
+// note: How can I make this more flexable for other users?
